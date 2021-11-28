68ac8b7fea98ef619a7afdcb6e88b9a496a45aac
Alexander Gostilo <alexandergostilo@gmail.com>
2021-11-28 22:32:25 +0300

    fix task2 and task3 lesson3

--- src/com/tms/lesson3/Task3.java
+++ src/com/tms/lesson3/Task3.java
@@ -9,11 +9,7 @@ public class Task3 {
         int[] array = {5, 2, -8, 12, 11, 9, 0, -5, 4};
         int diff = 0;
         for (int i = 0; i < array.length; i++) {
-            if (i % 2 == 0) {
-                diff += array[i];
-            } else {
-                diff -= array[i];
-            }
+            diff = (i % 2 == 0) ? diff + array[i] : diff - array[i];
         }
 
         System.out.println(diff);
